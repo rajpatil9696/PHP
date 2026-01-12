032834d060a55104ceb229d32b00748af2dd1fbf
Raj Patil <pshivraj9696@gmail.com>
2026-01-12 12:46:42 +0530

    added index and variable files

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  echo "I love Pizza <br>";
+  echo "pizza is awesome";
+
+  // this is a comment 
+  /*
+   this is multo line comment 
+   yess hwll yeah 
+  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button> Order Pizza </button>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,14 @@
+<?php
+  $name="raj patil <br>";
+  echo $name ;
+  $mail="raajpatil9696@gmail.com";
+  $age=21;
+  $price =4.99;
+  echo "{$age} <br>";
+  echo "hello {$name} <br>";
+  echo "my email is {$mail} <br>";
+  
+  echo "pizza is \${$price}";
+
+  $available =true ;
+?>
